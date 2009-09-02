9301489d53bc2ee6cc451facf12b5473a0585678
SSDany <inadsence@gmail.com>
2009-09-02 13:04:22 +0400

    Utils#detect_best_charset spec

--- spec/utils/detect_best_charset_spec.rb
+++ spec/utils/detect_best_charset_spec.rb
@@ -2,8 +2,46 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Rack::Acceptable::Utils, ".detect_best_charset" do
 
-  it "figures out which charsets are acceptable" do
-    pending "coming soon :)"
+  it "figures out which encodings are acceptable (in definitive cases)" do
+
+    helper = lambda do |provides, *accepts|
+      Rack::Acceptable::Utils.detect_best_charset(provides, accepts)
+    end
+
+    pending "standard samples" do
+
+      helper[%w(iso-8859-1)                         , ['*', 0.0]                          ].should == nil
+      helper[%w(iso-8859-1)                         , ['*', 1.0]                          ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1)             , ['*', 0.0]                          ].should == nil
+      helper[%w(unicode-1-1 iso-8859-1)             , ['*', 1.0]                          ].should == 'unicode-1-1'
+      helper[%w(unicode-1-1)                        , ['*', 0.0]                          ].should == nil
+      helper[%w(unicode-1-1)                        , ['*', 1.0]                          ].should == 'unicode-1-1'
+
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-1', 0.0]                 ].should == nil
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-1', 0.5]                 ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-1', 1.0]                 ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-5', 0.0], ['*', 0.0]     ].should == nil
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-5', 0.3], ['*', 0.5]     ].should == 'unicode-1-1'
+      helper[%w(unicode-1-1 iso-8859-1 iso-8859-5)  , ['iso-8859-5', 1.0], ['*', 0.5]     ].should == 'iso-8859-5'
+
+    end
+
+  end
+
+  it "figures out which encodings are acceptable (when iso-8859-1 and wildcard is NOT explicitly mentioned)" do
+
+    pending "RFC-ish shamanism with iso-8859-1" do
+
+      helper[%w(unicode-1-1 iso-8859-1)             , ['unicode-1-1', 0.0]                ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1)             , ['unicode-1-1', 0.3]                ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1)             , ['unicode-1-1', 1.0]                ].should == 'unicode-1-1' # more specific
+
+      helper[%w(unicode-1-1 iso-8859-1)             , ['iso-8859-5', 0.0]                 ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1)             , ['iso-8859-5', 0.3]                 ].should == 'iso-8859-1'
+      helper[%w(unicode-1-1 iso-8859-1)             , ['iso-8859-5', 1.0]                 ].should == 'iso-8859-1'
+
+    end
+
   end
 
 end
